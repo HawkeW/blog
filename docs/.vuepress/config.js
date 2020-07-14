6404175ab42194350eb932d6a54a377b8d2b6e5c
hughew <hughew@foxmail.com>
2020-07-14 10:38:04 +0800

    update:修改配置config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    title: 'Once° 一度',
+    title: 'Once° 一度', //博客标题，浏览器标签显示的内容
     theme: 'reco',
     description:'Once I live, once I aim',
     // base:'/blog/',
     head:[
-      ['link', { rel: 'icon', href: '/favicon.ico' }],
-      ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+      ['link', { rel: 'icon', href: '/favicon.ico' }],//浏览器标签上显示的icon
+      ['meta', { name: 'theme-color', content: '#3eaf7c' }],//主题颜色
       ['link', { rel: 'manifest', href: '/manifest.json' }],
     ],
     themeConfig:{
       type:'blog',
-      authorAvatar: '/avatar.jpg',
-      logo: '/logo.jpg',
-      author: '一度',
-      sidebar: 'auto',
-      sidebarDepth:3,
+      authorAvatar: '/avatar.jpg',//博客作者的头像
+      logo: '/logo.jpg',//博客logo
+      author: '一度',//博客作者
+      sidebar: 'auto',//自动生成文章的侧边栏
+      sidebarDepth:3,//侧边栏生成到H3标题
       valineConfig: {
         appId: 'bvN3KWqAgpLl13jibG4RMbFM-gzGzoHsz',// your appId
         appKey: 'YH74CiOC6QspPUOsbqSvakEP' // your appKey
@@ -50,13 +50,13 @@ module.exports = {
     plugins:[
         ["@vuepress/back-top"],
         ["@vuepress/nprogress"],
-        ["@vuepress-reco/vuepress-plugin-bgm-player",{
-          audios: [
+        ["@vuepress-reco/vuepress-plugin-bgm-player",{//音乐播放插件
+          audios: [//每一个中括号就是一个音乐对象
             {
-              name: 'Home~in this corner~',
-              artist: 'Leina',
-              url: '/assets/music/1.mp3',
-              cover: '/assets/music/1.jpg'
+              name: 'Home~in this corner~',//该音乐的名称
+              artist: 'Leina',//该音乐的作者
+              url: '/assets/music/1.mp3',//该音乐的地址，当前为/blog/.vuepress/public/assets/music/1.mp3
+              cover: '/assets/music/1.jpg'//该音乐的图片
             },
           ],
         }],
